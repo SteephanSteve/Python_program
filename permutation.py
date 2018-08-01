c79deebc3c310911f8832ff85923539f6ed86aa0
SteephanSteve <40833438+SteephanSteve@users.noreply.github.com>
2018-08-01 20:27:13 +0530

    Update permutation.py

--- permutation.py
+++ permutation.py
@@ -2,13 +2,16 @@
 
 def perm(s,f,l):
    if f==l:
-      print ''.join(s)
+      x=''.join(s)
+      if x not in a:
+         a.append(x)
    else:
       for i in range(f,l+1):
          s[i],s[f]=s[f],s[i]
          perm(s,f+1,l)
          s[f],s[i]=s[i],s[f]
 s=list(raw_input())
+a=[]
 perm(s,0,len(s)-1)
-   
-
+for i in a:
+   print i
